302f4a468b7046171442fdb2d6ccf40a6ab1a3b1
Bartosz Machniak <b.machniak01@gmail.com>
2025-05-20 21:32:41 +0200

    Minor fixes

--- finance-entries/finance-entries.js
+++ finance-entries/finance-entries.js
@@ -41,12 +41,6 @@ const handleTypeClick = () => {
 }
 
 const presentRecords = (records) => {
-    // Record = {
-    //      id, 
-    //      desc, 
-    //      value, 
-    //      date 
-    // }
     let builder = ""
     for (record of records) {
         builder += 
@@ -62,9 +56,10 @@ const presentRecords = (records) => {
 }
 
 const formatDate = (date) => {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
+    const dateObj = new Date(date);
+    const year = dateObj.getFullYear();
+    const month = String(dateObj.getMonth() + 1).padStart(2, '0');
+    const day = String(dateObj.getDate()).padStart(2, '0');
   
     return `${year}-${month}-${day}`;
 }
