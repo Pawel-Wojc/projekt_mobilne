c426e7fc5ffd0a56adbc0137a1b3cc5b965e8680
pawel-pawel <pawelitwojcik@gmail.com>
2025-05-20 21:11:26 +0200

    database update

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors')
+const cors = require('cors');
 const crypto = require('crypto');
 
 const app = express();
@@ -16,7 +16,7 @@ const CONFIG = require('../config.json');
 // Połączenie z bazą danych MongoDB
 mongoose.connect(CONFIG.mongoDB, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
@@ -28,10 +28,18 @@ db.once('open', () => {
 // Definicja schematu użytkownika
 const userSchema = new mongoose.Schema({
     username: String,
-    password: String
+    password: String,
 });
+
+const transactionSchema = new mongoose.Schema({
+    userId: String,
+    date: Date,
+    amount: Number,
+    description: String,
+});
+
 const User = mongoose.model('User', userSchema);
-console.log(User);
+const FinanceRecord = mongoose.model('FinanceRecord', transactionSchema);
 
 app.use(bodyParser.json());
 
@@ -39,33 +47,39 @@ app.use(bodyParser.json());
 app.post('/api/register', async (req, res) => {
     let { username, password } = req.body;
     // hashing password
-    password = crypto.createHash('sha256', password)
-    .update(CONFIG.salt)
-    .digest('base64');
+    password = crypto
+        .createHash('sha256', password)
+        .update(CONFIG.salt)
+        .digest('base64');
 
     try {
         const existingUser = await User.findOne({ username });
- 
+
         if (existingUser) {
-            return res.status(400).json({ message: 'Użytkownik już istnieje.' });
+            return res
+                .status(400)
+                .json({ message: 'Użytkownik już istnieje.' });
         }
         const newUser = new User({ username, password });
         await newUser.save();
         res.status(201).json({ message: 'Utworzono nowego użytkownika.' });
     } catch (error) {
         console.error('Błąd podczas tworzenia użytkownika:', error);
-        res.status(500).json({ message: 'Wystąpił błąd podczas tworzenia użytkownika.' });
+        res.status(500).json({
+            message: 'Wystąpił błąd podczas tworzenia użytkownika.',
+        });
     }
 });
 
-app.post('/api/login', async(req, res) =>{
+app.post('/api/login', async (req, res) => {
     let username = req.get('username');
     let password = req.get('password');
     // hashing password
-    password = crypto.createHash('sha256', password)
-    .update(CONFIG.salt)
-    .digest('base64');
-  
+    password = crypto
+        .createHash('sha256', password)
+        .update(CONFIG.salt)
+        .digest('base64');
+
     try {
         const existingUser = await User.findOne({ username });
         if (existingUser.password !== password) {
@@ -79,8 +93,25 @@ app.post('/api/login', async(req, res) =>{
     }
 });
 
+app.get('/api/summary', async (req, res) => {
+    currentUser = localStorage.getItem('currentUser');
+    if (!currentUser) {
+        return res.status(401).json({ message: 'Nieautoryzowany dostęp' });
+    }
+
+    try {
+        const userRecords = await FinanceRecord.find({ userId: currentUser });
+        res.status(200).json(userRecords);
+    } catch (error) {
+        console.error('Błąd podczas pobierania podsumowania:', error);
+        res.status(500).json({
+            message: 'Wystąpił błąd podczas pobierania podsumowania.',
+        });
+    }
+});
+
 // Start serwera
 app.listen(PORT, () => {
-   // console.log(`process.env.PORT: ${process.env.PORT}`);
+    // console.log(`process.env.PORT: ${process.env.PORT}`);
     console.log(`Serwer nasłuchuje na porcie ${PORT}.`);
 });
